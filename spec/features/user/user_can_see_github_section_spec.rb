ad52439bf4cc8222e8b4c1c823af6ef9d28ea4c1
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-01-30 10:21:50 -0700

    Passes first feature test which tests for classes appearing.
    Co-authored-by: Mary Goodhart <40919519+mgoodhart5@users.noreply.github.com>

--- spec/features/user/user_can_see_github_section_spec.rb
+++ spec/features/user/user_can_see_github_section_spec.rb
@@ -7,10 +7,25 @@ describe 'As a logged in user' do
     stub_get_json(url, filename)
 
     user = create(:user)
-    allow_any_instance_of(ApplicationController).to receive(user).and_return(current_user)
+
+    visit '/'
+
+    click_on "Sign In"
+
+    expect(current_path).to eq(login_path)
+
+    fill_in 'session[email]', with: user.email
+    fill_in 'session[password]', with: user.password
+
+    click_on 'Log In'
 
     expect(current_path).to eq(dashboard_path)
-    expect(page).to have_content("Github")
-  end
 
+    within ".github" do
+      expect(page).to have_content("Github")
+      within(first(".repo")) do
+        expect(page).to have_css(".name")
+      end
+    end
+  end
 end
