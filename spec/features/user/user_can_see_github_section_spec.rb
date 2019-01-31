74ea73e304051f1a9099c04f50d3e8c2b04a3c9c
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-01-31 07:26:02 -0700

    Adds to testing

--- spec/features/user/user_can_see_github_section_spec.rb
+++ spec/features/user/user_can_see_github_section_spec.rb
@@ -2,66 +2,67 @@ require 'rails_helper'
 
 describe 'As a logged in user' do
   it 'When I visit /dashboard I see a Github section for Repos' do
-    filename = "github_repos.json"
-    url = "https://api.github.com/user/repos"
-    stub_get_json(url, filename)
+    VCR.use_cassette("user_can_create_account_then_see_repo") do
+      filename = "github_repos.json"
+      url = "https://api.github.com/user/repos"
+      stub_get_json(url, filename)
 
-    user = create(:user)
+      user = create(:user)
 
-    visit '/'
+      visit '/'
 
-    click_on "Sign In"
+      click_on "Sign In"
 
-    expect(current_path).to eq(login_path)
+      expect(current_path).to eq(login_path)
 
-    fill_in 'session[email]', with: user.email
-    fill_in 'session[password]', with: user.password
+      fill_in 'session[email]', with: user.email
+      fill_in 'session[password]', with: user.password
 
-    click_on 'Log In'
+      click_on 'Log In'
 
-    expect(current_path).to eq(dashboard_path)
+      expect(current_path).to eq(dashboard_path)
 
-    within ".github" do
-      expect(page).to have_content("Github")
-      within(first(".repos")) do
-        expect(page).to have_css(".repo_name")
-        expect(page).to have_link("little_shop")
+      within ".github" do
+        expect(page).to have_content("Github")
+        within(first(".repos")) do
+          expect(page).to have_css(".repo_name")
+          expect(page).to have_link("little_shop")
+        end
       end
     end
   end
 
   it 'When I visit /dashboard I see a Github section for Followers' do
-    filename = "github_followers.json"
-    url = "https://api.github.com/user/followers"
-    stub_get_json(url, filename)
+    VCR.use_cassette("user_can_create_account_then_see_followers") do
+      filename = "github_followers.json"
+      url = "https://api.github.com/user/followers"
+      stub_get_json(url, filename)
 
-    user = create(:user)
+      user = create(:user)
 
-    visit '/'
+      visit '/'
 
-    click_on "Sign In"
+      click_on "Sign In"
 
-    expect(current_path).to eq(login_path)
+      expect(current_path).to eq(login_path)
 
-    fill_in 'session[email]', with: user.email
-    fill_in 'session[password]', with: user.password
+      fill_in 'session[email]', with: user.email
+      fill_in 'session[password]', with: user.password
 
-    click_on 'Log In'
+      click_on 'Log In'
 
-    expect(current_path).to eq(dashboard_path)
+      expect(current_path).to eq(dashboard_path)
 
-    within(first(".followers")) do
-      expect(page).to have_content("Followers")
-      expect(page).to have_link("handle_name")
+      within ".github" do
+        expect(page).to have_content("Followers")
+      end
+
+      within(first(".followers")) do
+        expect(page).to have_link("abroberts5")
+      end
     end
   end
 
 end
 
-
-# As a logged in user
-# When I visit /dashboard
-# Then I should see a section for "Github"
-# ------------------------------------------------------------------------
-# And under that section I should see another section titled "Followers"
 # And I should see list of all followers with their handles linking to their Github profile
