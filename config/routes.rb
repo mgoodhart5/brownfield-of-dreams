42235064c391cafa1f24a85efd4137e911a4fdf1
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-02-02 16:31:07 -0700

    Comments out route that is not hand rolled in Connect to GitHub button.
    Co-authored-by: Mary Goodhart 
    <40919519+mgoodhart5@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'tags/:tag', to: 'welcome#index', as: :tag
   get '/register', to: 'users#new'
-  get '/auth/github', as: :github_login
+  # get '/auth/github', as: :github_login
   get '/auth/github/callback', to: "github_sessions#create"
 
   namespace :admin do
