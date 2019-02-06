93e8adeaa325a7e19ecf6bb97f0372f7b33d2603
Mary Goodhart <mary.loves.pry@gmail.com>
2019-02-06 12:50:23 -0700

    logic for tutorial show on welcome page.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,17 @@
 class WelcomeController < ApplicationController
   def index
-    if params[:tag]
-      @tutorials = Tutorial.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
-    else
-      @tutorials = Tutorial.all.paginate(:page => params[:page], :per_page => 5)
+    if current_user
+      if params[:tag]
+        @tutorials = Tutorial.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
+      else
+        @tutorials = Tutorial.all.paginate(:page => params[:page], :per_page => 5)
+      end
+    elsif
+      if params[:tag]
+        @tutorials = Tutorial.non_classroom_tutorials.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
+      else
+        @tutorials = Tutorial.non_classroom_tutorials.paginate(:page => params[:page], :per_page => 5)
+      end
     end
   end
 end
--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -2,4 +2,9 @@ class Tutorial < ApplicationRecord
   has_many :videos, ->  { order(position: :ASC) }
   acts_as_taggable_on :tags, :tag_list
   accepts_nested_attributes_for :videos
+
+  def self.non_classroom_tutorials
+    Tutorial.where(classroom: false)
+  end
+  
 end
