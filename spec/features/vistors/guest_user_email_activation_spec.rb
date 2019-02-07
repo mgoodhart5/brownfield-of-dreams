4ac747af4fc452671047c386545489adb5530575
Mary Goodhart <mary.loves.pry@gmail.com>
2019-02-07 13:28:37 -0700

    passing tests, one test skipped because it is for next card

--- spec/features/vistors/guest_user_email_activation_spec.rb
+++ spec/features/vistors/guest_user_email_activation_spec.rb
@@ -48,23 +48,3 @@ describe 'vister can create an account' do
     expect(user.status).to eq(true)
   end
 end
-
-# As a guest user
-# When I visit "/"
-# And I click "Register"
-# Then I should be on "/register"
-# And when I fill in an email address (required)
-# And I fill in first name (required)
-# And I fill in first name (required)
-# And I fill in password and password confirmation (required)
-# And I click submit
-# Then I should be redirected to "/dashboard"
-# And I should see a message that says "Logged in as <SOME_NAME>"
-# And I should see a message that says "This account has not yet been activated. Please check your email."
-# As a non-activated user
-# When I check my email for the registration email
-# I should see a message that says "Visit here to activate your account."
-# And when I click on that link
-# Then I should be taken to a page that says "Thank you! Your account is now activated."
-# And when I visit "/dashboard"
-# Then I should see "Status: Active"
--- spec/models/following_spec.rb
+++ spec/models/following_spec.rb
@@ -25,3 +25,14 @@ describe '.class methods' do
     expect(Following.find_all_followings(user).count).to eq(30)
   end
 end
+describe '#instance methods' do
+  it '#app_user?' do
+    create(:user, github_id: "nick0lass")
+    name = "nick0lass"
+    email = "email@gmail.com"
+    attributes = {login: name, gh_email: email}
+    following = Following.new(attributes)
+
+    expect(following.app_user?).to eq(true)
+  end
+end
