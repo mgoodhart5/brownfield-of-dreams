2b081cb65787096c492df1008b9f29cd55b346a0
Mary Goodhart <mary.loves.pry@gmail.com>
2019-02-07 17:26:09 -0700

    finishes logic and methods and currently passing tests for sending an invite.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,7 @@ class SearchController < ApplicationController
   def show
     invite_info = Invite.find_email(params[:github_id], current_user)
     if invite_info.email
-      UserMailer.invite_email(invite_info)
+      UserMailer.invite_email(invite_info).deliver
       flash[:notice] = "Successfully sent invite!"
     else
       flash[:notice] = "The Github user you selected doesn't have an email address associated with their account."
